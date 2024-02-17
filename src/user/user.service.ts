be1518c507de92fe7a3210dce6f91511d4f00ae5
Juancapp <juan.cappuccio@radiumrocket.com>
2024-02-17 19:26:34 -0300

    edit getUser service to create user when it does not exist

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
 import { User } from './user.schema';
@@ -15,10 +15,70 @@ export class UserService {
     return response;
   }
 
-  async getUser(id: string): Promise<User> {
-    const user = await this.userModel.findById(id);
+  async getUser(name: string): Promise<User> {
+    const user = await this.userModel.findOne({
+      name: name.toLocaleLowerCase(),
+    });
 
-    if (!user) throw new NotFoundException('User not found');
-    return user;
+    if (user) {
+      return user;
+    } else {
+      const createdUser = await this.userModel.create({ name: name });
+      return createdUser;
+    }
+  }
+
+  async addOneItem(userId: string, productId: string): Promise<User> {
+    return this.userModel
+      .findOneAndUpdate(
+        { _id: userId, 'products.product': { $ne: productId } },
+        { $push: { products: { product: productId, quantity: 1 } } },
+        { new: true },
+      )
+      .exec()
+      .then((user) => {
+        if (user) return user;
+        return this.userModel
+          .findOneAndUpdate(
+            { _id: userId, 'products.product': productId },
+            { $inc: { 'products.$.quantity': 1 } },
+            { new: true },
+          )
+          .exec();
+      });
+  }
+
+  async removeOneItem(userId: string, productId: string): Promise<User> {
+    return this.userModel
+      .findOneAndUpdate(
+        {
+          _id: userId,
+          'products.product': productId,
+          'products.quantity': { $gt: 1 },
+        },
+        { $inc: { 'products.$.quantity': -1 } },
+        { new: true },
+      )
+      .exec()
+      .then((user) => {
+        if (user) return user;
+        return this.userModel
+          .findOneAndUpdate(
+            { _id: userId },
+            { $pull: { products: { product: productId } } },
+            { new: true },
+          )
+          .exec();
+      });
+  }
+
+  async removeAllItems(userId: string, productId: string): Promise<User> {
+    return this.userModel
+      .findOneAndUpdate(
+        { _id: userId },
+        { $pull: { products: { product: productId } } },
+        { new: true },
+      )
+      .exec();
   }
 }
--- src/user/users.controller.ts
+++ src/user/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Param } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, Patch } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.schema';
 
@@ -7,18 +7,42 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @Post()
-  async createProduct(
+  async createUser(
     @Body()
     user,
   ): Promise<User> {
     return this.userService.createUser(user);
   }
 
-  @Get(':productId')
-  async findByProductId(
-    @Param('productId')
-    id: string,
+  @Get(':userName')
+  async findUser(
+    @Param('userName')
+    name: string,
   ): Promise<User> {
-    return this.userService.getUser(id);
+    return this.userService.getUser(name);
+  }
+
+  @Patch('add/:userId/:productId')
+  async addOneItem(
+    @Param('userId') userId: string,
+    @Param('productId') productId: string,
+  ): Promise<User> {
+    return this.userService.addOneItem(userId, productId);
+  }
+
+  @Patch('remove/:userId/:productId')
+  async removeOneItem(
+    @Param('userId') userId: string,
+    @Param('productId') productId: string,
+  ): Promise<User> {
+    return this.userService.removeOneItem(userId, productId);
+  }
+
+  @Patch('removeAll/:userId/:productId')
+  async removeAllItems(
+    @Param('userId') userId: string,
+    @Param('productId') productId: string,
+  ): Promise<User> {
+    return this.userService.removeAllItems(userId, productId);
   }
 }
