317c3f8724d37380b94f75e5b5324662b3962ed3
Juancapp <juan.cappuccio@radiumrocket.com>
2024-02-25 15:35:34 -0300

    edit vercel json

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-  app.enableCors();
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(3000);
 }
-
 bootstrap();
